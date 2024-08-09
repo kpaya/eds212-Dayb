5d7240673da5a83869ce391c891ab5f31603ed0b
kpaya <karolpaya@hotmail.com>
2024-08-09 14:41:18 -0700

    "practice"

---
+++ intro-logicals.R
@@ -0,0 +1,27 @@
+#comparison operators
+
+2<5
+5<2
+"A"<"D"
+"Z"<"Q"
+
+#C < D , therefore true
+"cat"<"dog"
+
+2<=5
+
+burritos <- c(-4,10,1,-5,9)
+tacos <- c(5,3,8,-5,6)
+tacos <= burritos
+
+5==TRUE
+
+5 != 6
+"cat" != "dog"
+
+#add some logicals
+5 < 10 & "cat" < "cat" < "dog"
+
+#not equal
+
+!5 == 6
